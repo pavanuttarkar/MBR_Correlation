8fdd187c54a5534e629876151010bfa41c66a7a9
Devansh Shukla <48386249+devanshshukla99@users.noreply.github.com>
2021-01-15 20:22:11 +0530

    modified build files

--- source/make_cython.sh
+++ source/make_cython.sh
@@ -1,5 +1,9 @@
 #rm *.o
 #rm *.so
-python setup_header_Fring.py build_ext --inplace
-python setup_header_read.py build_ext --inplace
-python setup_header_geometric.py build_ext --inplace
+#python setup_header_Fring.py build_ext --inplace
+#python setup_header_read.py build_ext --inplace
+#python setup_header_geometric.py build_ext --inplace
+rm call_to_read_dev.cpp
+rm header_Fringe_Search.c
+rm geometric.c
+python2 setup.py build_ext --inplace
