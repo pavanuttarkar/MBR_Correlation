b2c36c3cd556e370539953e47b83f8ecb3988e80
pavanuttarkar <pavan.uttarkar@gmail.com>
2021-11-27 20:59:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,36 +14,3 @@ setup(
                Extension('api._header_delay_cy', ['CYTHON/delay.pyx'],language ="c++",)], \
   cmdclass={'build_ext': build_ext, 'embedsignature': True},
 )
-
-'''
-setup(
-  name='Cython moduel 01',
-  ext_modules=[Extension('_header_Fring_cy', {'embedsignature': True}, ['header_Fringe_Search_proc.pyx'],language ="c++",)],
-  cmdclass={'build_ext': build_ext},
-)
-
-setup(
-  name='Cython moduel 02',
-  ext_modules=[Extension('_header_read_cy', {'embedsignature': True}, ['call_to_read_dev_dev_dev.pyx'],language ="c++",)],
-  cmdclass={'build_ext': build_ext},
-)
-
-setup(
-  name='Cython moduel 03',
-  ext_modules=[Extension('_header_geometric_cy', {'embedsignature': True}, ['geometric.pyx'],language ="c++",)],
-  cmdclass={'build_ext': build_ext},
-)
-
-
-setup(
-  name='Cython moduel 04',
-  ext_modules=[Extension('_header_gps_cy', {'embedsignature': True}, ['header_gps_dev.pyx'],language ="c++",)],
-  cmdclass={'build_ext': build_ext},
-)
-
-setup(
-  name='Cython moduel 05',
-  ext_modules=[Extension('_header_delay_cy', {'embedsignature': True}, ['delay.pyx'],language ="c++",)],
-  cmdclass={'build_ext': build_ext},
-)
-'''
